e1dedf6757eb26986d1c0511163dee9745abeeed
Amit Sama <amitsama.tum@gmail.com>
2025-06-26 17:46:33 -0400

    Course12, Module1, Task2

--- server/frontend/src/components/Header/Header.jsx
+++ server/frontend/src/components/Header/Header.jsx
@@ -3,25 +3,28 @@ import "../assets/style.css";
 import "../assets/bootstrap.min.css";
 
 const Header = () => {
+    /***
+     */
+    
     const logout = async (e) => {
-    e.preventDefault();
-    let logout_url = window.location.origin+"/djangoapp/logout";
-    const res = await fetch(logout_url, {
-      method: "GET",
-    });
-  
-    const json = await res.json();
-    if (json) {
-      let username = sessionStorage.getItem('username');
-      sessionStorage.removeItem('username');
-      window.location.href = window.location.origin;
-      window.location.reload();
-      alert("Logging out "+username+"...")
-    }
-    else {
-      alert("The user could not be logged out.")
-    }
-  };
+        e.preventDefault();
+        let logout_url = window.location.origin+"/djangoapp/logout";
+        const res = await fetch(logout_url, {
+            method: "GET",
+        });
+        
+        const json = await res.json();
+        if (json) {
+            let username = sessionStorage.getItem('username');
+            sessionStorage.removeItem('username');
+            window.location.href = window.location.origin;
+            window.location.reload();
+            alert("Logging out "+username+"...")
+        }
+        else {
+            alert("The user could not be logged out.")
+        }
+    };
     
 //The default home page items are the login details panel
 let home_page_items =  <div></div>
